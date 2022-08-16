8a3eb41150a60550d58c8fcddf62769f16ca6470
yang6626 <305020042@qq.com>
2022-08-16 17:22:13 +0800

    add daily timer

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./routers/routes.ts";
+import { cron } from "https://deno.land/x/deno_cron/cron.ts";
 
 const app = new Application();
 const PORT = 4002;
@@ -10,4 +11,9 @@ app.use(router.allowedMethods());
 app.addEventListener("listen", (_) =>
   console.log(`app runing at port ${PORT}`)
 );
+
+/** 每日15:10 工作日 */
+cron("0 10 15 * * 1-5", () => {
+  console.log("timer");
+});
 await app.listen({ port: PORT });
