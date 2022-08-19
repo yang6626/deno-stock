cd476c4a37ea3817c54cbe23b914d5d7ab759631
yang6626 <305020042@qq.com>
2022-08-19 10:30:55 +0800

    fix crod bug

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./routers/routes.ts";
-import { every15Minute } from "https://deno.land/x/deno_cron/cron.ts";
+import { every15Minute } from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
 
 const app = new Application();
 const PORT = 4002;
