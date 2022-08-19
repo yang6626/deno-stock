f0b0520eda495cd4cca7ee968cd17cf877d8086c
yang6626 <63634780+yang6626@users.noreply.github.com>
2022-08-19 10:14:51 +0800

    remove unuse import

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./routers/routes.ts";
-import { cron, every15Minute } from "https://deno.land/x/deno_cron/cron.ts";
+import { every15Minute } from "https://deno.land/x/deno_cron/cron.ts";
 
 const app = new Application();
 const PORT = 4002;
